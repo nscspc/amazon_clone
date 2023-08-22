d5f9344f2e30cd295c1cbb8e70a0e4873260ae2f
nscspc <saininaveen933@gmail.com>
2023-08-22 11:46:15 +0530

    created adminMiddleware
    created admin.js file
    
    created product model
    
    created admin_services
    
    added image picking keys in info.plist for ios

---
+++ lib/features/admin/services/admin_services.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/constants/utils.dart';
+import 'package:flutter_amazon_clone/models/product.dart';
+
+class AdminServices {
+  void sellProduct({
+    required BuildContext context,
+    required String name,
+    required String description,
+    required double price,
+    required double quantity,
+    required String category,
+    required List<File> images,
+  }) async {
+    try {
+      final cloudinary = CloudinaryPublic("dgklzqfkh", "pz6ro6ck");
+      List<String> imageUrls = [];
+
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(images[i].path,
+              folder:
+                  name), // folder will be created in cloudinary ji by the name of the product.
+        );
+        imageUrls.add(res.secureUrl);
+      }
+
+      Product product = Product(
+        name: name,
+        description: description,
+        quantity: quantity,
+        images: imageUrls,
+        category: category,
+        price: price,
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+}
